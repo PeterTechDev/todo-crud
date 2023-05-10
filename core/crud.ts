4fa82edfb216d350874048e7bae5289e92f156c7
peter <peterleite.dev@gmail.com>
2023-05-09 21:39:47 -0300

    Add a create function and start creating an object for demonstration.

--- core/crud.ts
+++ core/crud.ts
@@ -1,15 +1,26 @@
 import fs from "fs"; // ES6
 // const fs = require("fs"); - CommonJS
-const DB_FILE_PATH = "./core/db";
+const DB_FILE_PATH = "./core/database";
 
 console.log("[CRUD]");
 
 function create(content: string) {
-  // salvar o content no sistema
-  fs.writeFileSync(DB_FILE_PATH, content);
+  const todo = {
+    date: new Date().toISOString(),
+    content: content,
+  };
+
+  console.log(todo);
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify(todo));
   return content;
 }
 
+function read() {
+  // ler o conteúdo do sistema
+  return fs.readFileSync(DB_FILE_PATH, "utf-8");
+}
 
 // [SIMULATION]
-console.log(create("Bora filhao"));
+create("Hello World");
+console.log(read());
