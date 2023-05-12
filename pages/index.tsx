039dcf990d314cdbd1b108c15681f4d5356eab76
peter <peterleite.dev@gmail.com>
2023-05-12 17:33:04 -0300

    initial setup with nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="container">
+      <h1>Hello, Next.js!</h1>
+      <h3>Is this the real life? Is this just fantasy?</h3>
+    </div>
+  )
+}
