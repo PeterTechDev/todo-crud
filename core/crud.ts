0e897437c2b1cc3b23b3d4d373e33372c8f1b69e
peter <peterleite.dev@gmail.com>
2023-05-14 21:04:30 -0300

    incrementing todos code

--- core/crud.ts
+++ core/crud.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
-import { log } from "console";
 import fs from "fs"; // ES6
 import { v4 as uuidv } from "uuid";
 // const fs = require("fs"); - CommonJS
 const DB_FILE_PATH = "./core/database";
 
-console.log("[CRUD]");
+// console.log("[CRUD]");
 
 interface Todo {
   id: string;
@@ -37,7 +36,7 @@ function create(content: string): Todo {
   return todo;
 }
 
-function read(): Array<Todo> {
+export function read(): Array<Todo> {
   // ler o conteúdo do sistema
   const databaseString = fs.readFileSync(DB_FILE_PATH, "utf-8");
   const database = JSON.parse(databaseString || "{}");
@@ -105,14 +104,14 @@ function CLEAR_DATABASE() {
 }
 
 // [SIMULATION]
-CLEAR_DATABASE();
-create("beber água");
-create("ler livro");
-const thirdTodo = create("comer bolo");
-// console.log(thirdTodo.id);
+// CLEAR_DATABASE();
+// create("beber água");
+// create("ler livro");
+// const thirdTodo = create("comer bolo");
+// // console.log(thirdTodo.id);
 
-update(thirdTodo.id, {
-  content: "comer bolo de chocolate",
-  done: true,
-});
-// console.log(read());
+// update(thirdTodo.id, {
+//   content: "comer bolo de chocolate",
+//   done: true,
+// });
+// // console.log(read());
--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,10 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { read } from "@database-crud-todo";
 
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  response.status(200).json({
-    todos: [],
+  // eslint-disable-next-line no-console
+  console.log(request.method);
+  if (request.method === "GET") {
+    const ALL_TODOS = read();
+    response.status(200).json({
+      todos: ALL_TODOS,
+    });
+    return; // return is used to stop the execution of the function
+  }
+
+  response.status(405).json({
+    message: "Method not allowed",
   });
 }
