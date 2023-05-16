b9c6e9b208b553e59a90599f24e0a2e817806088
peter <peterleite.dev@gmail.com>
2023-05-16 04:21:03 -0300

    Add functionality to create a 'todo' through the frontend input, and implement error handling in the UI controller

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -23,7 +23,30 @@ function filterTodosByContent<Todo>(
   return homeTodos;
 }
 
+interface TodoControllerCreateParams {
+  content?: string;
+  onError: () => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onSuccess: (todo: any) => void;
+}
+function create({ content, onSuccess, onError }: TodoControllerCreateParams) {
+  // Fail Fast
+  if (!content) {
+    onError();
+    return;
+  }
+
+  const todo = {
+    id: "12345",
+    content,
+    date: new Date(),
+    done: false,
+  };
+  onSuccess(todo);
+}
+
 export const todoController = {
   get,
   filterTodosByContent,
+  create,
 };
